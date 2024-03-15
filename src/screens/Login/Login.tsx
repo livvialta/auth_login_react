821eed5cb6154ed55a452c5c032cf5cb3ee0aa9d
LucasGill <lucasgilsilva@gmail.com>
2024-03-14 22:28:51 -0300

    clean error in login page

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,105 +1,101 @@
 import React, { useState } from "react";
-import { ActivityIndicator, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import {FIREBASE_AUTH} from  '../../../fireBaseconfig';
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { ActivityIndicator, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from "react-native";
+import { FIREBASE_AUTH } from "../../../firebaseConfig";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
-    
-    const [ email, setEmail ] = useState('');
-    const [ password, setPassword ] = useState('');
-    const [ loading, setLoading ] = useState(false);
 
-const auth = FIREBASE_AUTH;
-
-const handleLogin = async () => {
-    setLoading(true);
-    try{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    //referencia para o obj de autenticação
+    const auth = FIREBASE_AUTH;
+    const handleLogin = async () => {
+      setLoading(true);
+      try {
         const authResponse = await signInWithEmailAndPassword(auth, email, password);
         console.log(authResponse);
-}catch(error: any){
-    console.log(error);
-    alert("Erro de autentificação" + error.message)
-}finally{
-    setLoading(false);
-}
-
-const handleNewAccount = () => {
-    setLoading(true);
-    try{
-        const responseCreate =
-            await createUserWithEmailAndPassword(auth, email, password)
-        console.log(responseCreate)
-    }catch(error){
-        
-
-    }finally{
-
+      }
+      catch(error : any){
+        console.error(error);
+        alert('Erro de autenticacao:' + error.message)
+      }
+      finally {
+        setLoading(false);
+      }
     }
-}
+    const handleNewAccont = async () => {
+      setLoading(true);
+      try {
+        const responseCreate = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(responseCreate);
+      }
+      catch(error : any){
+        console.error(error);
+        alert('Erro de criação de conta:' + error.message)
+      }
+      finally {
+        setLoading(false);
+      }
+    }
     return(
-        <View style={styles.container}>
-            <TextInput
-                style={styles.input}
-                placeholder="Email"
-                autoCapitalize="none"
-                value={email}
-                onChangeText={ (text) => setEmail(text) }
-                />
-            <TextInput
-                style={styles.input}
-                placeholder="Password"
-                autoCapitalize="none"
-                value={password}
-                onChangeText={ (text) => setPassword(text) }
-                secureTextEntry={true}
-                />
-            { loading ? 
-                (<ActivityIndicator />) : 
-                (<View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.buttonClick} onpress = {handleLogin}>
-                        <Text style={styles.buttonText}>Login</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.buttonClick} onpress = {handleNewAccount}>
-                        <Text style={styles.buttonText}>Create account</Text>
-                    </TouchableOpacity>
-                </View>)
-            }
-        </View>
+      <View style={styles.container}>
+        <TextInput
+        style={styles.input}
+        placeholder='Email'
+        autoCapitalize='none'
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+        style={styles.input}
+        placeholder='Password'
+        autoCapitalize='none'
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        />
+      {loading ? (<ActivityIndicator/>) : (<View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.buttonClick} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonClick} onPress={handleNewAccont}>
+        <Text style={styles.buttonText}>Create account</Text>
+        </TouchableOpacity>
+      </View>)}
+      </View>
     );
 }
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        marginHorizontal: 20
-    },
-    input: {
-        marginVertical: 4,
-        height: 50,
-        borderWidth: 1,
-        borderRadius: 4,
-        padding: 10,
-        backgroundColor: '#fff',
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingVertical: 10,
-    },
-    buttonClick: {
-        backgroundColor: '#006db2',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 20,
-        flex: 1
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 16,
-        textAlign: 'center'
-    }
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      marginHorizontal: 20
+  },
+  input: {
+      marginVertical: 4,
+      height: 50,
+      borderWidth: 1,
+      borderRadius: 4,
+      padding: 10,
+      backgroundColor: '#fff',
+  },
+  buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingVertical: 10,
+  },
+  buttonClick: {
+      backgroundColor: '#006db2',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 20,
+      flex: 1
+  },
+  buttonText: {
+      color: 'white',
+      fontSize: 16,
+      textAlign: 'center'
+  }
 });
-
 export default Login;
